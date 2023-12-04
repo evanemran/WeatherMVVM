dc7e80466ae1c14ea602d6a61f3ba65da4f26038
evanemran <evanemran007@gmail.com>
2023-12-04 22:15:34 +0600

    forecast components added

--- app/src/main/java/com/evanemran/weathermvvm/MainActivity.kt
+++ app/src/main/java/com/evanemran/weathermvvm/MainActivity.kt
@@ -1,28 +1,41 @@
 package com.evanemran.weathermvvm
 
 import android.Manifest
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import com.evanemran.weathermvvm.presentation.WeatherCard
+import com.evanemran.weathermvvm.presentation.WeatherForecast
 import com.evanemran.weathermvvm.presentation.WeatherViewModel
 import com.evanemran.weathermvvm.presentation.ui.theme.DarkBlue
 import com.evanemran.weathermvvm.presentation.ui.theme.DeepBlue
 import com.evanemran.weathermvvm.presentation.ui.theme.WeatherMVVMTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
     private val viewModel: WeatherViewModel by viewModels()
     private lateinit var permissionLauncher: ActivityResultLauncher<Array<String>>
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         permissionLauncher = registerForActivityResult(
@@ -36,12 +49,29 @@ class MainActivity : ComponentActivity() {
         ))
         setContent {
             WeatherMVVMTheme {
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(DarkBlue)
+                Box(
+                    modifier = Modifier.fillMaxSize()
                 ) {
-                    WeatherCard(state = viewModel.state, backgroundColor = DeepBlue)
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(DeepBlue)
+                    ) {
+                        WeatherCard(state = viewModel.state, backgroundColor = Color.LightGray)
+                        Spacer(modifier = Modifier.height(16.dp))
+                        WeatherForecast(state = viewModel.state)
+                    }
+                    if(viewModel.state.isLoading) {
+                        CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+                    }
+                    viewModel.state.error?.let { error ->
+                        Text(
+                            text = error,
+                            color = Color.Red,
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier.align(Alignment.Center)
+                        )
+                    }
                 }
             }
         }
